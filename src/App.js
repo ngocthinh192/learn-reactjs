25e99d53900c3610cfc5bb7b32fa674cddd2c9cc
Ngoc Thinh <thinhtn.itedu@gmail.com>
2022-06-03 16:44:26 +0700

    Date 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Thinh';
+  const age = 18;
+  const isMale = false;
+  const student = {
+    name: "best"
+  };
+  const colorList = ['red', 'blue', 'yellow']
+
+  const listStudent = ['Nhu', 'trang', 'quyen', 'duoc']
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +20,31 @@ function App() {
         <p>
           Hello WORLD
         </p>
+
+        <p>
+          My name is : {name} - i'm {age} - {isMale ? 'Male' : 'Famale'} - my favorite color is Blue - but i'm not {student.name}
+        </p>
+
+        {
+          isMale && (
+            <ul>
+              {colorList.map(
+                color => (<li style={{ color }}>{color}</li>)
+              )}
+            </ul>
+          )
+        }
+
+        {
+          !isMale && (
+            <ul>
+
+              {listStudent.map(
+                nameStudent => (<li>{nameStudent}</li>)
+              )}
+            </ul>
+          )
+        }
 
       </header>
     </div>
